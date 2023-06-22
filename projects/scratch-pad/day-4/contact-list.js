933e1249b05f8057eb57575019bfbec5f4268756
Jessica Benitez <benitez.jessica731@gmail.com>
2023-06-22 20:21:14 +0000

    save work in contact list

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -20,7 +20,7 @@
  *         returns the contact object if found in the contacts-list, or, 
  *         undefined if the fullName does not match any contacts in the list.
  *      4. removeContact(contact): takes a contact object to be removed from 
- *         the contact-list.
+ *         the contact-list. //research splice method
  *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
  *         return a String formated with all the full-names of the separated 
  *         with a line-break, like so:
@@ -34,8 +34,12 @@
  */
 
 // YOUR CODE GOES BELOW HERE //
-function makeContact(id, nameFirst, nameLast) {
-
+function makeContact(id, nameFirst, nameLast) { //MAKE SURE TO GET THIS PASSING FIRST
+    return {
+        id: id,
+        nameFirst: nameFirst,
+        nameLast: nameLast
+    }
 } 
 
 
@@ -43,12 +47,28 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        },
+        addContact: function(contact) {
+            contacts.push(contact);
+        },
+        findContact: function(fullName) {
+            if (contacts.nameFirst + " " + contacts.nameLast === fullName) {
+                return contact;
+            } else {
+                return undefined;
+            }
+        },
+        removeContact: function(contact) {
+            contacts.splice(contact, 1);
+        },
+        printAllContactNames: function(){
+            
         }
     }
 }
